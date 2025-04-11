1a160c9f04293048d3f71155de906357528a9e07
german-zaracho <germanzarachodev@gmail.com>
2025-04-10 22:43:05 -0300

    some style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,9 +5,9 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, items }) => {
     return (
-        <div className="bg-white rounded-2xl shadow-md p-4 mb-4">
-            <h2 className="text-xl font-bold mb-2">{title}</h2>
-            <ul className="list-disc list-inside space-y-1 text-sm text-gray-700 text-left">
+        <div className="group bg-white hover:bg-[#FFC229] rounded-[30px] shadow-[inset_0_-12px_6px_#ADCFFF] hover:shadow-[inset_0_-6px_8px_#FF6314] font-medium tracking-[-0.24px] px-5 py-4 m-[10px]">
+            <h2 className="text-xl font-bold mb-2 text-black group-hover:text-[#C80036]">{title}</h2>
+            <ul className="list-disc list-inside space-y-1 text-sm text-gray-700 text-left group-hover:text-black">
                 {items.map((item, index) => (
                     <li key={index} dangerouslySetInnerHTML={{ __html: item }} />
                 ))}

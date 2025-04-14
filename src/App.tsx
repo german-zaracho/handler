8680405caabe316634c6b7523d8de3e2f8f299c6
german-zaracho <germanzarachodev@gmail.com>
2025-04-13 23:06:48 -0300

    adding a search input

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import data from './data/cards.json';
 type CardData = {
   title: string;
   items: string[];
+  tags: string[];
 };
 
 function App() {
 
+  const [filter, setFilter] = useState<string>('');
   const [cards, setCards] = useState<CardData[]>([]);
 
   useEffect(() => {
@@ -17,15 +19,27 @@ function App() {
     setCards(data);
   }, []);
 
+  const filteredCards = cards.filter(card =>
+    card.tags.some(tag => tag.toLowerCase().includes(filter))
+  );
+
   return (
     <>
       <h1 className='test'>My Handler</h1>
 
+      <input
+        type="text"
+        placeholder="Buscar por tag..."
+        value={filter}
+        onChange={(e) => setFilter(e.target.value.toLowerCase())}
+        className="border border-gray-300 rounded-lg px-4 py-2 mb-4 w-full md:w-1/2"
+      />
+
       <div className="p-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-      {cards.map((card, index) => (
-        <Card key={index} title={card.title} items={card.items} />
-      ))}
-    </div>
+        {filteredCards.map((card, index) => (
+          <Card key={index} title={card.title} items={card.items} />
+        ))}
+      </div>
 
     </>
   )
